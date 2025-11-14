20337973194a1ca140db95536d96da50247c4cbf
szalai.mate <szalai.mate@students.jedlik.eu>
2025-11-14 07:59:57 +0100

    incomplete popup handler

---
+++ BlazeUI/Popups/Popup.cs
@@ -0,0 +1,8 @@
+﻿using Avalonia.Controls;
+
+namespace BlazeUI.Popups;
+
+public class Popup : Grid
+{
+    public bool Active = false;
+}
---
+++ BlazeUI/Popups/PopupHandler.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Controls;
+
+namespace BlazeUI.Popups;
+
+public class PopupHandler : Grid
+{
+    public void Init()
+    {
+        ZIndex = -5;
+        
+    }
+}
