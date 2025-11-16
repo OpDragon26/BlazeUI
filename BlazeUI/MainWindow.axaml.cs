94d5ac4e79fa396d1a373220f1f4e5b9c30a2717
OpDragon26 <25opdragon25@gmail.com>
2025-11-16 11:20:12 +0100

    Revert "terminal entries"
    This reverts commit a69c429a6731e7b65cb754d4dda926bcea8393b4.

--- BlazeUI/MainWindow.axaml.cs
+++ BlazeUI/MainWindow.axaml.cs
@@ -135,11 +135,6 @@ public partial class MainWindow : Window
     {
         PopupHandlerGrid.ClearActive();
     }
-
-    private void OpenTerminal(object? sender, RoutedEventArgs e)
-    {
-        PopupHandlerGrid.SetActive("TerminalPopup");
-    }
 }
 
 public static class Colors
